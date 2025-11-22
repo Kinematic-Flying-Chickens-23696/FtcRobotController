019358527c1a8df92cfbc3c2442b83302101cdf7
Lucas Williams <surfluke19@gmail.com>
2025-11-22 09:54:30 -0800

    Update auto programs.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NotVeryTuffAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NotVeryTuffAuto.java
@@ -1,27 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "NotVeryTuffAuto")
-public abstract class NotVeryTuffAuto extends OpMode {
+public class NotVeryTuffAuto extends LinearOpMode {
 	private DcMotor fl0;
 	private DcMotor fr1;
 	private DcMotor bl2;
 	private DcMotor br3;
 	private static final ElapsedTime runtime = new ElapsedTime();
 	@Override
-	public void init() {
+	public void runOpMode() {
 		fl0 = hardwareMap.get(DcMotor.class, "fl0");
 		fr1 = hardwareMap.get(DcMotor.class, "fr1");
 		bl2 = hardwareMap.get(DcMotor.class, "bl2");
 		br3 = hardwareMap.get(DcMotor.class, "br3");
-	}
-	
-	@Override
-	public void start() {
+		waitForStart();
 		fl0.setPower(0.5);
 		fr1.setPower(0.5);
 		bl2.setPower(0.5);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VeryTuffAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VeryTuffAuto.java
@@ -1,27 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.AutonomousMovement.Movement;
 
 @Autonomous(name = "VeryTuffAuto", group = "Autonomous")
-public abstract class VeryTuffAuto extends OpMode {
+public class VeryTuffAuto extends LinearOpMode {
 	//lightweight maxxing
 	Movement movement = new AutonomousMovement.Movement();
 	private DcMotor fl0;
 	private DcMotor fr1;
 	private DcMotor bl2;
 	private DcMotor br3;
+
 	@Override
-	public void init() {
+	public void runOpMode() {
 		fl0 = hardwareMap.get(DcMotor.class, "fl0");
 		fr1 = hardwareMap.get(DcMotor.class, "fr1");
 		bl2 = hardwareMap.get(DcMotor.class, "bl2");
 		br3 = hardwareMap.get(DcMotor.class, "br3");
+		
+		waitForStart();
+		
+		movement.drive(0.5, 1500);
 	}
-	
-	@Override
-	public void start() {movement.drive(0.5, 1500);}
 }
