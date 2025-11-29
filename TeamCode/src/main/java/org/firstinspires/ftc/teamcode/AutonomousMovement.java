87691d735edeb1e057ab7db42ed8ca7af2e17b50
Lucas Williams <surfluke19@gmail.com>
2025-11-29 15:53:29 -0800

    Revert "Refactor AutonomousMovement.java to be spread into multiple files, making it easier to tweak other values and functions."
    This reverts commit 151c493badbbfb0bd9f74a65558a4ac2bd24d091.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMovement.java
@@ -1,70 +1,164 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.helpers.LimelightHelper;
-import org.firstinspires.ftc.teamcode.helpers.ModuleHelper;
-import org.firstinspires.ftc.teamcode.helpers.MovementHelper;
 
-/**
- * AutonomousMovement class handles all robot functions during the autonomous period.
- * Intended to be imported into a LinearOpMode class for use.
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class AutonomousMovement {
 	private final Telemetry telemetry;
-	private final MovementHelper movementHelper;
-	private final ModuleHelper moduleHelper;
+	private final Limelight3A limelight;
+	private final DcMotor fl0;
+	private final DcMotor fr1;
+	private final DcMotor bl2;
+	private final DcMotor br3;
+	private final DcMotor shootmotor;
 	private final ElapsedTime runtime = new ElapsedTime();
+	private final Movement movementHelper = new Movement();
+	private final Module moduleHelper = new Module();
+	private final List<Integer> fiducialIds = new ArrayList<>();
+	private boolean isLimelightStarted;
 	
-	/**
-	 * Constructor for AutonomousMovement class.
-	 *
-	 * @param hardwareMap HardwareMap object from the OpMode.
-	 * @param telemetry   Telemetry object from the OpMode.
-	 */
 	public AutonomousMovement(HardwareMap hardwareMap, Telemetry telemetry) {
 		this.telemetry = telemetry;
-		Limelight3A limelight = hardwareMap.get(Limelight3A.class, "ll3a");
-		DcMotor fl0 = hardwareMap.get(DcMotor.class, "fl0");
-		DcMotor fr1 = hardwareMap.get(DcMotor.class, "fr1");
-		DcMotor bl2 = hardwareMap.get(DcMotor.class, "bl2");
-		DcMotor br3 = hardwareMap.get(DcMotor.class, "br3");
-		DcMotor shootmotor = hardwareMap.get(DcMotor.class, "shootmotor");
-		
-		this.movementHelper = new MovementHelper(fl0, fr1, bl2, br3, runtime, telemetry);
-		LimelightHelper limelightHelper = new LimelightHelper(limelight);
-		this.moduleHelper = new ModuleHelper(shootmotor, runtime, telemetry, limelightHelper);
+		this.limelight = hardwareMap.get(Limelight3A.class, "ll3a");
+		this.fl0 = hardwareMap.get(DcMotor.class, "fl0");
+		this.fr1 = hardwareMap.get(DcMotor.class, "fr1");
+		this.bl2 = hardwareMap.get(DcMotor.class, "bl2");
+		this.br3 = hardwareMap.get(DcMotor.class, "br3");
+		this.shootmotor = hardwareMap.get(DcMotor.class, "shootmotor");
 	}
 	
-	/**
-	 * Returns the MovementHelper object.
-	 *
-	 * @return MovementHelper object.
-	 */
-	public MovementHelper movement() {
+	public Movement movement() {
 		return movementHelper;
 	}
 	
-	/**
-	 * Returns the ModuleHelper object.
-	 *
-	 * @return ModuleHelper object.
-	 */
-	public ModuleHelper module() {
+	public Module module() {
 		return moduleHelper;
 	}
 	
-	/**
-	 * Displays the remaining runtime on telemetry.
-	 *
-	 * @param timeMs Total time in milliseconds.
-	 * @return The remaining time in milliseconds.
-	 */
-	public double runtimeRemaining(long timeMs) {
-		return timeMs - runtime.milliseconds();
+	public Limelight3A limelightDevice() {
+		return limelight;
+	}
+	
+	public void startLimelight() {
+		if (!isLimelightStarted) {
+			limelight.start();
+			isLimelightStarted = true;
+		}
+	}
+	
+	public void stopLimelight() {
+		if (isLimelightStarted) {
+			limelight.stop();
+			isLimelightStarted = false;
+		}
+	}
+	
+	public void runtimeRemaining(long timeMs) {
+		telemetry.addData("Runtime remaining: ", (timeMs - runtime.milliseconds()));
+		telemetry.update();
+	}
+	
+	public class Movement {
+		// "and when im rock hard i ******* rock hard" -ken ashcorp
+		public void drive(double speed, long timeMs) {
+			fl0.setPower(speed);
+			fr1.setPower(speed);
+			bl2.setPower(speed);
+			br3.setPower(speed);
+			runtime.reset();
+			while (runtime.milliseconds() < timeMs) {
+				runtimeRemaining(timeMs);
+			}
+			stop();
+		}
+		
+		public void slide(double speed, long timeMs) {
+			fl0.setPower(speed);
+			fr1.setPower(-speed);
+			bl2.setPower(-speed);
+			br3.setPower(speed);
+			runtime.reset();
+			while (runtime.milliseconds() < timeMs) {
+				runtimeRemaining(timeMs);
+			}
+			stop();
+		}
+		
+		public void rotation(double speed, long timeMs) {
+			fl0.setPower(speed);
+			fr1.setPower(-speed);
+			bl2.setPower(speed);
+			br3.setPower(-speed);
+			runtime.reset();
+			while (runtime.milliseconds() < timeMs) {
+				runtimeRemaining(timeMs);
+			}
+			stop();
+		}
+		
+		public void stop() {
+			fl0.setPower(0);
+			fr1.setPower(0);
+			bl2.setPower(0);
+			br3.setPower(0);
+		}
+	}
+	
+	public class Module {
+		// "tickle my fancy tickle my prostate" -ken ashcorp
+		private final Limelight limelightHelper = new Limelight();
+		
+		public void shooter(double speed, long timeMs) {
+			shootmotor.setPower(speed);
+			runtime.reset();
+			while (runtime.milliseconds() < timeMs) {
+				runtimeRemaining(timeMs);
+			}
+			shootmotor.setPower(0);
+		}
+		
+		public Limelight limelight() {
+			return limelightHelper;
+		}
+		
+		public class Limelight {
+			public List<Integer> getResults() {
+				fiducialIds.clear();
+				LLResult result = limelight.getLatestResult();
+				if (result != null && result.isValid()) {
+					List<LLResultTypes.FiducialResult> detectedTags = result.getFiducialResults();
+					for (LLResultTypes.FiducialResult tag : detectedTags) {
+						fiducialIds.add(tag.getFiducialId());
+					}
+				}
+				return Collections.unmodifiableList(new ArrayList<>(fiducialIds));
+			}
+			
+			public String getMonolithOrientation(List<Integer> detectedTags) {
+				LLResult result = limelight.getLatestResult();
+				if (result != null && result.isValid()) {
+					for (Integer tag : detectedTags) {
+						if (tag == 21) {
+							return "GREEN PURPLE PURPLE";
+						} else if (tag == 22) {
+							return "PURPLE GREEN PURPLE";
+						} else if (tag == 23) {
+							return "PURPLE PURPLE GREEN";
+						}
+					}
+				}
+				return null;
+			}
+		}
 	}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightAuto.java
@@ -4,24 +4,24 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.teamcode.helpers.LimelightHelper;
-import org.firstinspires.ftc.teamcode.helpers.ModuleHelper;
+import org.firstinspires.ftc.teamcode.AutonomousMovement.Module;
 
 @Autonomous(name = "LimelightAuto")
 public class LimelightAuto extends OpMode {
-	private final AutonomousMovement autonomousMovement = new AutonomousMovement(hardwareMap, telemetry);
-	ModuleHelper moduleHelper = autonomousMovement.module();
-	LimelightHelper limelightHelper = moduleHelper.limelight();
-	Limelight3A limelight = hardwareMap.get(Limelight3A.class, "ll3a");
+	private AutonomousMovement autonomousMovement;
 	
 	@Override
 	public void init() {
+		autonomousMovement = new AutonomousMovement(hardwareMap, telemetry);
+		Module module = autonomousMovement.module();
+		Module.Limelight limelightHelper = module.limelight();
+		Limelight3A limelight = autonomousMovement.limelightDevice();
 		limelight.pipelineSwitch(0);
 	}
 	
 	@Override
 	public void init_loop() {
-		limelightHelper.start();
+		autonomousMovement.startLimelight();
 	}
 	
 	@Override
@@ -31,7 +31,7 @@ public class LimelightAuto extends OpMode {
 	
 	@Override
 	public void stop() {
-		limelightHelper.stop();
+		autonomousMovement.stopLimelight();
 	}
 }
 
